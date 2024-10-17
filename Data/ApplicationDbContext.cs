14bd8f50b40f9b94091cbc8ca8c7bf1e4a09fe3a
NathulaDayarathne <nathuladayarathne@gmail.com>
2024-10-17 23:57:28 +0530

    Add ApplicationDbContext with DbSets for core entities

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RunHub.Models;
+using RunHub.Models;
+
+namespace RunHub.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<AppUser>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Race> Races { get; set; }
+		public DbSet<Club> Clubs { get; set; }
+		public DbSet<Address> Addresses { get; set; }
+		public DbSet<State> States { get; set; }
+		public DbSet<City> Cities { get; set; }
+	}
+}
--- Models/Race.cs
+++ Models/Race.cs
@@ -3,7 +3,7 @@ using RunHub.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RunGroopWebApp.Models
+namespace RunHub.Models
 {
 	public class Race
 	{
