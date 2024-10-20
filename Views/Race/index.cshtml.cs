ac48789b3f47a707a3dfc9dd088f71047d30adf1
NathulaDayarathne <nathuladayarathne@gmail.com>
2024-10-21 01:49:33 +0530

    add views for club and race

---
+++ Views/Race/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RunHub.Views.Race
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
