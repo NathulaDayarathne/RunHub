7b32655e0850fc434b3db923307798725374870d
NathulaDayarathne <nathuladayarathne@gmail.com>
2024-10-21 01:49:10 +0530

    add controllers for club and race

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunHub.Data;
+using RunHub.Models;
+namespace RunHub.Controllers
+{
+	public class ClubController: Controller
+	{
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()   //CCCCCCC
+		{
+            List<Club> clubs = _context.Clubs.ToList(); //MMMMMMM
+            return View(clubs);  //VVVVVV
+		}
+
+	}
+}
