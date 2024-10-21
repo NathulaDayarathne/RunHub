ca04d5651aaa8aa70e2d20507b71cf956723f69a
NathulaDayarathne <nathuladayarathne@gmail.com>
2024-10-21 18:00:51 +0530

    add controller for races

--- Controllers/RaceController.cs
+++ Controllers/RaceController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using RunHub.Data;
+using RunHub.Models;
 namespace RunHub.Controllers
 {
-	public class RaceController: Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
 
-	}
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()   //CCCCCCC
+        {
+            List<Race> races = _context.Races.ToList(); //MMMMMMM
+            return View(races);  //VVVVVV
+        }
+
+    }
 }
