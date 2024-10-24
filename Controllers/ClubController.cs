791190037642f8e8e3875d6f4524b7873ca4c4f6
NathulaDayarathne <nathuladayarathne@gmail.com>
2024-10-24 20:39:33 +0530

    Add form view and POST functionality for creating and saving club details to the database

--- Controllers/ClubController.cs
+++ Controllers/ClubController.cs
@@ -5,28 +5,46 @@ using RunHub.Interfaces;
 using RunHub.Models;
 namespace RunHub.Controllers
 {
-	public class ClubController: Controller
+	public class ClubController : Controller
 	{
-        
+
 		private readonly IClubRepository clubRepository;
 
 		public ClubController(IClubRepository clubRepository)
-        { 
+		{
 			this.clubRepository = clubRepository;
 		}
 
-        public async Task<IActionResult> Index()   //CCCCCCC
+		public async Task<IActionResult> Index()   //CCCCCCC
 		{
-            IEnumerable<Club> clubs = await clubRepository.GetAll(); //MMMMMMM
-            return View(clubs);  //VVVVVV
+			IEnumerable<Club> clubs = await clubRepository.GetAll(); //MMMMMMM
+			return View(clubs);  //VVVVVV
 		}
 
-        public async Task<IActionResult> Detail(int id)   //CCCCCCC
-        {
-            Club club = await clubRepository.GetByIdAsync(id); //MMMMMMM
+		public async Task<IActionResult> Detail(int id)   //CCCCCCC
+		{
+			Club club = await clubRepository.GetByIdAsync(id); //MMMMMMM
 
 			return View(club);  //VVVVVV
-        }
+		}
 
-    }
+		public IActionResult Create()   //CCCCCCC
+		{
+			//MMMMMMM
+			return View();  //VVVVVV
+		}
+
+		[HttpPost]
+
+		public async Task<IActionResult> Create(Club club)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(club);
+			}
+
+			clubRepository.Add(club);
+			return RedirectToAction("Index");
+		}
+	}
 }
