48c3199659e7f801792cbd88e35a4410a68a83f5
NathulaDayarathne <nathuladayarathne@gmail.com>
2024-10-24 16:43:07 +0530

    add club interface

---
+++ Interfaces/IClubRepository.cs
@@ -0,0 +1,23 @@
+﻿using RunHub.Models;
+
+namespace RunHub.Interfaces
+{
+	public interface IClubRepository
+	{
+		Task<IEnumerable<Club>> GetAll();
+
+		Task<Club> GetByIdAsync(int id);
+
+		Task<IEnumerable<Club>> GetClubCity(string city);
+
+		bool Add(Club club);
+		bool Update(Club club);	
+		bool Delete(Club club);	
+		bool Save();
+
+
+
+
+
+	}
+}
